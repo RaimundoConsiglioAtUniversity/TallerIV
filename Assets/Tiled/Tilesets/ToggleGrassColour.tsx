e3a2278807fcb7b3686cb3acc3e3bbf2c071ad19
Ryebuck Junction / Rai / SpaceFunk <90148699+SpaceFunkRai@users.noreply.github.com>
2025-03-26 19:11:29 -0300

    Started Implementation of Own Graphics

--- Assets/Tiled/Tilesets/ToggleGrassColour.tsx
+++ Assets/Tiled/Tilesets/ToggleGrassColour.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="ToggleGrass_Colours" tilewidth="16" tileheight="16" tilecount="49" columns="7">
- <image source="../../Sprites/tilesets/rocks_monochrome.png" width="112" height="112"/>
+ <image source="../../Sprites/tilesets/rocks.png" width="112" height="112"/>
  <tile id="0">
   <properties>
    <property name="unity:layer" value="Ground"/>
--- Assets/Tiled/Tilesets/ToggleGrassTops.tsx
+++ Assets/Tiled/Tilesets/ToggleGrassTops.tsx
@@ -3,7 +3,7 @@
  <image source="../../Sprites/tilesets/grass_tops.png" width="112" height="112"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="1" x="0" y="2" width="16" height="16"/>
   </objectgroup>
  </tile>
 </tileset>
